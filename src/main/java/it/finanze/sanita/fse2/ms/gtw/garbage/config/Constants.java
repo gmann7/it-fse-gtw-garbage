3ff1d4ac289585c248ea36c8520bfd60ebfdc131
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-09-22 12:26:47 +0200

    feat: Remove EDS flow

--- src/main/java/it/finanze/sanita/fse2/ms/gtw/garbage/config/Constants.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtw/garbage/config/Constants.java
@@ -16,7 +16,7 @@ package it.finanze.sanita.fse2.ms.gtw.garbage.config;
  */
 public final class Constants {
 	
-	public static final String FINAL_STATUS = "EDS_WORKFLOW";
+	public static final String FINAL_STATUS = "SEND_TO_INI";
 
 	
 	public static final class Collections {
